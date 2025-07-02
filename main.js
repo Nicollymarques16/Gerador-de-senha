431216c80f72adad546a4c5b7dfc710f52ba3349
Nicollymarques16 <159157738+Nicollymarques16@users.noreply.github.com>
2025-07-02 12:25:27 -0300

    02/07

---
+++ main.js
@@ -0,0 +1,66 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuirTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+};
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+};
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+for (let i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) alfabeto += letrasMaiusculas;
+    if (checkbox[1].checked) alfabeto += letrasMinusculas;
+    if (checkbox[2].checked) alfabeto += numeros;
+    if (checkbox[3].checked) alfabeto += simbolos;
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
+        senha += alfabeto[numeroAleatorio];
+    }
+
+    campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha() {
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (tamanhoSenha > 11) {
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5) {
+        forcaSenha.classList.add('media');
+    } else {
+        forcaSenha.classList.add('fraca');
+    }
+}
+
+// Inicializa com uma senha gerada
+geraSenha();
